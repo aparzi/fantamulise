5d17e7977d0f4e5279784ec5e631c0ad6abef6c9
aparziale <angelo.parziale@democom.it>
2021-03-27 22:47:59 +0100

    WIP

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 // You can delete this file if you're not using it
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `FantaMulise`,
+    description: `Website of the fantacalcio FantaMulise league.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -33,4 +33,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  pathPrefix: "/fantamulise"
 }
